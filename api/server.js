7a4d427614746855ea1a5632241aa60ddcd9c6a2
Allan Ayford <60179207+allanayford-dev@users.noreply.github.com>
2022-06-27 06:58:35 +0000

    Create server.js file.

---
+++ api/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express;
+
+app.get('/', (req, res) => {
+    res.send(200).json({ message: `API has been successfully created.` });
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`This application is running on ${process.env.HOST}:${process.env.PORT}`)
+})
